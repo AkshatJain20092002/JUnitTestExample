b7ef3bdb559c511cdb7089cc0c4935f99b59c44b
AkshatJain20092002 <E21CSEU0164@bennett.edu.in>
2022-11-18 16:22:59 +0530

    JUnitTestExample

---
+++ src/JUnitTestExamplePackage/JUnitTesting.java
@@ -0,0 +1,12 @@
+package JUnitTestExamplePackage;
+
+public class JUnitTesting {
+	public int AddNumbers(int a,int b) {
+		int ans=a+b;
+		return ans;
+	}
+	public String AddStrings(String a,String b) {
+		String ans=a+b;
+		return ans;
+	}
+}
